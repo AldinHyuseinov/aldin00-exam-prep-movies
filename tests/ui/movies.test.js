6ef7f3030c7a03543d8aeaeb86c2b06b458afa14
AldinHyuseinov <Aldin6687@gmail.com>
2023-12-07 16:28:16 +0200

    fixed some ui tests url

--- tests/ui/movies.test.js
+++ tests/ui/movies.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check movies page', async ({ page }) => {
-    await page.goto('https://aldin00-movies.onrender.com');
+    await page.goto('https://aldin00-movies.onrender.com/movies');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
